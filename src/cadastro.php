a78019be25b9925032e3d6d9c8cb376281190fb0
JoaoCurtarelli7 <joao.vcb@aluno.ifsc.edu.br>
2021-04-18 02:49:11 -0300

    ew

--- src/cadastro.php
+++ src/cadastro.php
@@ -35,64 +35,94 @@ include "./head.php"
 ?>
 
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
+
 <head>
-<meta charset = "UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<h3>Formulário</h3>
+    <h3>Formulário</h3>
 
     <form action="cadastro.php" method="post">
         <input type="hidden" name="id" styles="width: 500px" value="<?php echo !empty($result->id)
 ? $result->id : ""; ?>"><br>
 
-        <label for="">Nome</label>
-        <input type="text" name="nome" id="" value="<?php echo !empty($result->nome)
-? $result->nome : ""; ?>"><br>
+        <div class="input-group mb-3">
 
-<label for="">Sobrenome</label>
-        <input type="text" name="sobrenome" id="" value="<?php echo !empty($result->sobrenome)
-? $result->sobrenome : ""; ?>" required><br>
+            <input type="text" class="form-control" placeholder="Nome" aria-describedby="basic-addon1" name="nome" id=""
+                value="<?php echo !empty($result->nome)
+? $result->nome : ""; ?>" required>
+        </div><br>
 
-        <label for="">Telefone 01</label>
-        <input type="text" name="telefone1" id="" value="<?php echo !empty($result->telefone1)
-? $result->telefone1 : ""; ?>" required><br>
 
-<div class="input-group mb-3">
-  <select class="custom-select" name="tipo_telefone1" id="" value="<?php echo !empty($result->tipo_telefone1)
+
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Sobrenome" aria-describedby="basic-addon1"
+                name="sobrenome" id="" value="<?php echo !empty($result->sobrenome)
+? $result->sobrenome : ""; ?>" required>
+        </div><br>
+
+
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Telefone 01" aria-describedby="basic-addon1"
+                name="telefone1" id="" value="<?php echo !empty($result->telefone1)
+? $result->telefone1 : ""; ?>" required>
+        </div>
+
+        <div class="input-group mb-3">
+            <select style="width: 500px;" class="custom-select" name="tipo_telefone1" id="" value="<?php echo !empty($result->tipo_telefone1)
 ? $result->tipo_telefone1 : ""; ?>" required>
 
- <option selected>Tipo Telefone 01</option>
-    <option value="Casa">Casa</option>
-    <option value="Celular">Celular</option>
-    <option value="Comercial">Comercial</option>
-    <option value="Principal">Principal</option>
-  </select>
-  <div class="input-group-append">
-    <label class="input-group-text" for="inputGroupSelect02">Opções</label>
-  </div>
-</div>
+                <option selected>Tipo Telefone 01</option>
+                <option value="Casa">Casa</option>
+                <option value="Celular">Celular</option>
+                <option value="Comercial">Comercial</option>
+                <option value="Principal">Principal</option>
+            </select>
+            <div class="input-group-append">
+                <label class="input-group-text" for="inputGroupSelect02">Opções</label>
+            </div>
+        </div><br>
+
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Telefone 02" aria-describedby="basic-addon1"
+                name="telefone2" id="" value="<?php echo !empty($result->telefone2)
+? $result->telefone2 : ""; ?>" required>
+        </div>
+
+        <div class="input-group mb-3">
+            <select style="width: 500px;" class="custom-select" name="tipo_telefone2" id="" value="<?php echo !empty($result->tipo_telefone2)
+? $result->tipo_telefone2 : ""; ?>" required>
+
+                <option selected>Tipo Telefone 02</option>
+                <option value="Casa">Casa</option>
+                <option value="Celular">Celular</option>
+                <option value="Comercial">Comercial</option>
+                <option value="Principal">Principal</option>
+            </select>
+            <div class="input-group-append">
+                <label class="input-group-text" for="inputGroupSelect02">Opções</label>
+            </div>
+        </div>
 
 
-<label for="">Telefone 02</label>
-        <input type="text" name="telefone2" id="" value="<?php echo !empty($result->telefone2)
-? $result->telefone2 : ""; ?>" required><br>
 
-<label for="">Tipo Telefone 02</label>
-        <input type="text" name="tipo_telefone2" id="" value="<?php echo !empty($result->tipo_telefone2)
-? $result->tipo_telefone2 : ""; ?>" required><br>
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="E-mail" aria-describedby="basic-addon1" name="email"
+                id="" value="<?php echo !empty($result->email)
+? $result->email : ""; ?>" required>
+        </div><br>
 
-<label for="">E-mail</label>
-        <input type="text" name="email" id="" value="<?php echo !empty($result->email)
-? $result->email : ""; ?>" required><br>
 
         <input type="submit" value="Salvar">
         <a href="./lista.php">Voltar</a>
     </form>
 </body>
+
 </html>
 
 <?php
--- src/lista.php
+++ src/lista.php
@@ -21,44 +21,46 @@ include "./head.php"
 ?>
 
 
-    <h4>Listagem de Usuários</h4>
+<h4>Listagem de Usuários</h4>
 
-    <form action="cadastro.php" method="post">
-        <input type="text" name="valor" id="">
+<form action="cadastro.php" method="post">
+    <input type="text" name="valor" id="">
 
-        <select name="tipo" id="">
-            <option value="nome">Nome</option>
-            <option value="sobrenome">Sobrenome</option>
-            <option value="telefone1">Telefone 02</option>
-            <option value="tipo_telefone1">Tipo telefone 01</option>
-            <option value="telefone2">Telefone 02</option>
-            <option value="tipo_telefone2">Tipo telefone 02</option>
-            <option value="email">E-mail</option>
-        </select>
-        <input type="submit" value="Buscar">
-    </form>
-    
+    <select name="tipo" id="">
+        <option value="nome">Nome</option>
+        <option value="sobrenome">Sobrenome</option>
+        <option value="telefone1">Telefone 02</option>
+        <option value="tipo_telefone1">Tipo telefone 01</option>
+        <option value="telefone2">Telefone 02</option>
+        <option value="tipo_telefone2">Tipo telefone 02</option>
+        <option value="email">E-mail</option>
+    </select>
+    <input type="submit" value="Buscar">
 </form>
 
-<a href = './cadastro.php' >Cadastrar</a>
-    
+</form>
 
-    <table class="table table-hover">
+<a href='./cadastro.php'> Cadastrar</a>
+
+
+<table class="table table-hover">
     <thead>
-    <th scope="col">ID</th>
-    <th scope="col">Nome</th>
-    <th scope="col">Sobrenome</th>
-    <th scope="col">Telefone 01</th>
-    <th scope="col">Tipo Telefone 01</th>
-    <th scope="col">Telefone 02</th>
-    <th scope="col">Tipo Telefone 02</th>
-    <th scope="col">Email</th>
+        <th scope="col">ID</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Sobrenome</th>
+        <th scope="col">Telefone 01</th>
+        <th scope="col">Tipo Telefone 01</th>
+        <th scope="col">Telefone 02</th>
+        <th scope="col">Tipo Telefone 02</th>
+        <th scope="col">Email</th>
+        <th scope="col">Ação</th>
+        <th scope="col">Ação</th>
 
 
 
     </thead>
     <tbody>
-    <?php
+        <?php
 foreach ($result as $item) {
     $item = (object) $item;
     echo "
@@ -71,14 +73,15 @@ foreach ($result as $item) {
         <td>" . $item->telefone2 . "</td>
         <td>" . $item->tipo_telefone2 . "</td>
         <td>" . $item->email . "</td>
-        <td><a href = 'cadastro.php?id=" . $item->id . "'>Editar</a></td>
-        <td><a href='lista.php?id=" . $item->id . "' onclick=\"return confirm('Deseja remover o registro ?'); \"  >Remover</a> </td>        </tr>
+        <td><a href = 'cadastro.php?id=" . $item->id . "'><i class=\"fas fa-user-edit\"></i></a></td>
+        <td><a href='lista.php?id=" . $item->id . "' onclick=\"return confirm('Deseja remover o registro ?'); \"  > <i class=\"fas fa-user-times\"></i></a> </td>       
+         </tr>
         ";
 }
 ?>
-</tbody>
-    </table>
+    </tbody>
+</table>
 
-    <?php
+<?php
 include "./footer.php"
 ?>
